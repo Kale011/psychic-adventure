cf2a9ca056f9d41cd77edade8ed8adfb0ea23c5e
Kale011 <coloneldanders@gmail.com>
2024-07-20 20:27:40 -0400

    My portfolio site

---
+++ Portfolio/script.js
@@ -0,0 +1,18 @@
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector('#message');
+
+let submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let emailValue = emailElement.value;
+    let messageValue = messageElement.value;
+
+    if(emailValue.includes('@')) {
+        alert('thank you for your message');
+    } else {
+        alert('Please enter a valid email address.');
+    }
+})
+
+
